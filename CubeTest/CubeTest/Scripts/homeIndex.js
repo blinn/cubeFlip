5bb90875071b3ca9aa387d8bfacdf759c740c0bb
Brett Linn <br3ttlinn@gmail.com>
2014-04-25 16:24:11 -0400

    removed cube content for menu testing

---
+++ CubeTest/CubeTest/Scripts/homeIndex.js
@@ -0,0 +1,13 @@
+﻿hexaUtil.createGrids(4);
+
+//Page events
+$(".cubeGroupRow").click(function () {
+    var arr = [];
+    var rowNum = $(this).data("cuberow");
+    arr.push(rowNum);
+    hexaUtil.rotateRow(arr);
+})
+
+$(".cubeGroupCol").click(function () {
+    hexaUtil.rotateCol($(this).data("cubecol"));
+})
